ea966ab65d0697aafc3d5f08069cd9cc5b07ba65
Ilya <punisher007@ya.ru>
2023-11-24 12:25:48 +0300

    add comments for methods

--- generator.py
+++ generator.py
@@ -5,6 +5,16 @@ import secrets
 
 class Generator:
     def __init__(self, length: int, lower: bool, number: bool, symbol: bool, upper: bool = True):
+         """
+        Initializes a Generator object with the specified parameters.
+
+        Args:
+            length (int): The length of the password.
+            lower (bool): Determines if lowercase letters should be included.
+            number (bool): Determines if numbers should be included.
+            symbol (bool): Determines if symbols should be included.
+            upper (bool, optional): Determines if uppercase letters should be included. Defaults to True.
+        """
         self.__length = length  # длина пароля
         self.__is_upper = upper  # будут ли буквы с верхним регистром
         self.__is_lower = lower  # будут ли буквы с нижн регистром
@@ -12,6 +22,12 @@ class Generator:
         self.__is_symbols = symbol  # будут ли символы(punctuation)
 
     def generate(self):
+          """
+        Generates a password based on the specified parameters.
+
+        Returns:
+            str: The generated password.
+        """
         chars = self.__get_character_set()
 
         while True:
@@ -23,6 +39,13 @@ class Generator:
         return password
 
     def __get_character_set(self):
+         """
+        Returns a list of characters based on the specified parameters.
+
+        Returns:
+            list: The list of characters.
+        """
+
         chars = []
 
         if not any([self.__is_upper, self.__is_lower, self.__is_numbers, self.__is_symbols]):
@@ -40,9 +63,27 @@ class Generator:
         return chars
 
     def __generate_password(self, chars):
+         """
+        Generates a password using the provided character set.
+
+        Args:
+            chars (list): The character set to use for generating the password.
+
+        Returns:
+            str: The generated password.
+        """
         return ''.join(secrets.choice(chars) for _ in range(self.__length))
 
     def __meets_requirements(self, password):
+         """
+        Checks if the generated password meets the requirements.
+
+        Args:
+            password (str): The generated password.
+
+        Returns:
+            bool: True if the password meets the requirements, False otherwise.
+        """
         if self.__is_upper and not any(c.isupper() for c in password):
             return False
         if self.__is_numbers and not any(c.isdigit() for c in password):
